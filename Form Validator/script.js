e5006740c38fe79c162bf0b62c230dee6c4bbcb1
mistahpig <dspark410@gmail.com>
2020-10-07 10:23:50 -0700

    adds validation to fields

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -16,18 +16,62 @@ function showError(input,message) {
 function showSuccess(input) {
     const formControl = input.parentElement
     formControl.className = 'form-control success'
+}
 
+//Check email validation
+function isValidEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(re.test(input.value.trim())){
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid')
+    }
     
+}
+
+//Check required fields
+const checkRequired = (inputArr) => {
+    inputArr.forEach(input => {
+        //console.log(input)
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`)
+        }else {
+            showSuccess(input)
+        }
+    })
+}
+
+//getFieldName input.id first letter to uppercase
+function getFieldName(input) {
+    return input.id.toUpperCase().charAt(0) + input.id.slice(1)
+}
+
+//Check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input)
+    }
+}
+
+//Check passwords match
+function passwordMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match')
+    }
 }
 
 //Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if (username.value === '') {
-        showError(username, 'Username is required')
-        console.log(username)
-    } else {
-        showSuccess(username)
-    }
+    checkRequired([username, email, password, password2])
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    isValidEmail(email)
+    passwordMatch(password,password2)
 })
