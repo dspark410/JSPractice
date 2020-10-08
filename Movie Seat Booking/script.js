8b644dc6a667213e9ed41b55c649fb108ee4b3c2
mistahpig <dspark410@gmail.com>
2020-10-08 15:35:34 -0700

    fixes bug

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -17,12 +17,15 @@ function setMovieData(movieIndex, moviePrice) {
 //Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
-    console.log(selectedSeats)
+
+    //console.log(selectedSeats)
+
     //Copy selected seats into arr
     //Map through aray
     //return new array indexes
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
 
+    
     localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 
     const selectedSeatsCount = selectedSeats.length
@@ -44,7 +47,6 @@ function populateUI() {
     }
 
     const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
-
     if (selectedMovieIndex !== null) {
         movieSelect.selectedIndex = selectedMovieIndex
     }
